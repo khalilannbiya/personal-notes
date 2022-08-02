8fe8a30ee63e16c6c7c54a6d0c0cf23e5cf3d7c3
khalilannbiya <syeichkhalil@gmail.com>
2022-08-02 12:57:48 +0700

    feat: create component NoteItem

--- src/components/NotesList/index.jsx
+++ src/components/NotesList/index.jsx
@@ -1,4 +1,5 @@
 import { showFormattedDate } from "../../utils/index";
+import { NoteItem } from "../index ";
 const NotesList = ({ notes, searchInput, resultBySearch, handleDelete, handleArsip }) => {
    return (
       <>
@@ -13,21 +14,22 @@ const NotesList = ({ notes, searchInput, resultBySearch, handleDelete, handleArs
                        {
                           return (
                              !note.archived && (
-                                <div key={note.id} className="note-item">
-                                   <div className="note-item__content">
-                                      <h3 className="note-item__title">{note.title}</h3>
-                                      <p className="note-item__date">{showFormattedDate(note.createdAt)}</p>
-                                      <p className="note-item__body">{note.body}</p>
-                                   </div>
-                                   <div className="note-item__action">
-                                      <button onClick={() => handleDelete(note.id)} className="note-item__delete-button">
-                                         Delete
-                                      </button>
-                                      <button onClick={() => handleArsip(note.id)} className="note-item__archive-button">
-                                         {note.archived ? "Pindahkan" : "Arsipkan"}
-                                      </button>
-                                   </div>
-                                </div>
+                                <NoteItem key={note.id} note={note} handleDelete={handleDelete} handleArsip={handleArsip} />
+                                // <div key={note.id} className="note-item">
+                                //    <div className="note-item__content">
+                                //       <h3 className="note-item__title">{note.title}</h3>
+                                //       <p className="note-item__date">{showFormattedDate(note.createdAt)}</p>
+                                //       <p className="note-item__body">{note.body}</p>
+                                //    </div>
+                                //    <div className="note-item__action">
+                                //       <button onClick={() => handleDelete(note.id)} className="note-item__delete-button">
+                                //          Delete
+                                //       </button>
+                                //       <button onClick={() => handleArsip(note.id)} className="note-item__archive-button">
+                                //          {note.archived ? "Pindahkan" : "Arsipkan"}
+                                //       </button>
+                                //    </div>
+                                // </div>
                              )
                           );
                        }
--- src/components/index .jsx
+++ src/components/index .jsx
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import FormCreateNote from "./FormCreateNote";
 import NoteInput from "./NoteInput";
 import NotesList from "./NotesList";
+import NoteItem from "./NoteItem";
 
-export { Navbar, FormCreateNote, NoteInput, NotesList };
+export { Navbar, FormCreateNote, NoteInput, NotesList, NoteItem };
